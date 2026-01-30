04873a7e24c667296ca36aa33958669c3f50bbf2
MadsWorsoe <31693592+MadsWorsoe@users.noreply.github.com>
2026-01-30 14:50:55 +0100

    Delete draft/ml directory

--- draft/ml/phase.py
+++
@@ -1,8 +0,0 @@
-﻿# draft/encoding/phase.py
-
-def get_draft_phase(sequence_number: int) -> str:
-    if sequence_number < 6:
-        return "EARLY"
-    elif sequence_number < 14:
-        return "MID"
-    return "LATE"
